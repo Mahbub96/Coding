82917b0a3efd4070cb4499d1fa184ee2ff5f1a70
Md Mahbub Alam <mahbub.alam.sobuz@gmail.com>
2022-07-27 22:09:07 -0700

    Create Assignment1.java

---
+++ Coding/java/Assignment1.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int value = 123456;
+        int n = 2;
+        
+        String s = String.valueOf(value);
+        System.out.println(s.charAt(n));
+        
+    }
+}
